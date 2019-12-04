8cea13b156af51e128a6768117cddd47011f97ba
Vzaa <Vzaa@users.noreply.github.com>
2019-12-04 10:57:38 +0300

    Day 4 update 1

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,4 +1,4 @@
-// Had this fn lying from excerism, it's in reverse order from what we need here though
+// Had this fn lying somewhere, it's in reverse order from what we need here though
 fn digits(n: i32) -> impl Iterator<Item = i32> {
     let mut state = n;
 
@@ -15,46 +15,46 @@ fn digits(n: i32) -> impl Iterator<Item = i32> {
 
 // Do the check in reverse here
 fn inc_check(n: i32) -> bool {
-    let mut d_iter = digits(n);
-    let mut d = d_iter.next().unwrap();
+    let mut d_iter = digits(n).peekable();
 
-    for d_next in d_iter {
-        if d < d_next {
-            return false;
+    while let Some(d) = d_iter.next() {
+        if let Some(&d_next) = d_iter.peek() {
+            if d < d_next {
+                return false;
+            }
         }
-        d = d_next;
     }
     true
 }
 
 fn adj_check(n: i32) -> bool {
-    let mut d_iter = digits(n);
-    let mut d = d_iter.next().unwrap();
+    let mut d_iter = digits(n).peekable();
 
-    for d_next in d_iter {
-        if d == d_next {
-            return true;
+    while let Some(d) = d_iter.next() {
+        if let Some(&d_next) = d_iter.peek() {
+            if d == d_next {
+                return true;
+            }
         }
-        d = d_next;
     }
     false
 }
 
 fn adj_check_2(n: i32) -> bool {
-    let mut d_iter = digits(n);
-    let mut d = d_iter.next().unwrap();
+    let mut d_iter = digits(n).peekable();
     let mut streak = 0;
 
-    for d_next in d_iter {
-        if d == d_next {
-            streak += 1;
-        } else {
-            if streak == 1 {
-                return true;
+    while let Some(d) = d_iter.next() {
+        if let Some(&d_next) = d_iter.peek() {
+            if d == d_next {
+                streak += 1;
+            } else {
+                if streak == 1 {
+                    return true;
+                }
+                streak = 0;
             }
-            streak = 0;
         }
-        d = d_next;
     }
 
     streak == 1
