6939ef4d7f89baf5a2319c2051e4654bb288876f
Vzaa <Vzaa@users.noreply.github.com>
2019-12-10 14:34:51 +0300

    Day 10

---
+++ day10/src/main.rs
@@ -0,0 +1,136 @@
+use std::collections::HashMap;
+use std::convert::{TryFrom, TryInto};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+type Pos = (i32, i32);
+type StarMap = HashMap<Pos, Spot>;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Spot {
+    Open,
+    Asteroid,
+}
+
+impl TryFrom<char> for Spot {
+    type Error = ();
+
+    fn try_from(v: char) -> Result<Self, Self::Error> {
+        let out = match v {
+            '.' => Spot::Open,
+            '#' => Spot::Asteroid,
+            _ => return Err(()),
+        };
+
+        Ok(out)
+    }
+}
+
+fn gcd(a: i32, b: i32) -> i32 {
+    if a == 0 {
+        b
+    } else {
+        gcd(b % a, a)
+    }
+}
+
+fn scale(a: Pos, s: i32) -> Pos {
+    (a.0 * s, a.1 * s)
+}
+
+fn div(a: Pos, d: i32) -> Pos {
+    (a.0 / d, a.1 / d)
+}
+
+fn diff(a: Pos, b: Pos) -> Pos {
+    (a.0 - b.0, a.1 - b.1)
+}
+
+fn add(a: Pos, b: Pos) -> Pos {
+    (a.0 + b.0, a.1 + b.1)
+}
+
+fn is_blocked(p: Pos, o: Pos, starmap: &StarMap) -> bool {
+    let v = diff(o, p);
+    let denom = gcd(v.0.abs(), v.1.abs());
+    let step = div(v, denom);
+    (1..denom) // all possible jumps
+        .map(|n| add(p, scale(step, n))) // get nth jump
+        .any(|v| starmap.get(&v) == Some(&Spot::Asteroid))
+}
+
+fn get_los(p: Pos, starmap: &StarMap) -> usize {
+    starmap
+        .iter()
+        .filter(|(&k, &v)| k != p && v == Spot::Asteroid && !is_blocked(p, k, &starmap))
+        .count()
+}
+
+fn calc_slope(p: Pos) -> (i32, f32) {
+    let y = -p.1 as f32; // want positive y to be up
+    let mut x = p.0 as f32;
+    if x == 0.0 {
+        x = 0.00001;
+    }
+
+    let abs_slope = y.abs() / x.abs();
+    if x >= 0.0 && y >= 0.0 {
+        (3, abs_slope)
+    } else if x >= 0.0 && y < 0.0 {
+        (2, abs_slope)
+    } else if x < 0.0 && y < 0.0 {
+        (1, -abs_slope)
+    } else {
+        (0, -abs_slope)
+    }
+}
+
+fn ordered_hitlist(p: Pos, starmap: &StarMap) -> Vec<Pos> {
+    let mut all: Vec<_> = starmap
+        .iter()
+        .filter(|(&k, &v)| k != p && v == Spot::Asteroid && !is_blocked(p, k, &starmap))
+        .map(|a| *a.0)
+        .collect();
+
+    all.sort_by(|a, b| {
+        let v_a = diff(*a, p);
+        let v_b = diff(*b, p);
+        calc_slope(v_b).partial_cmp(&calc_slope(v_a)).unwrap()
+    });
+
+    all
+}
+
+fn main() {
+    let rdr = BufReader::new(File::open("input").unwrap());
+    let mut starmap: HashMap<Pos, Spot> = HashMap::new();
+
+    for (y, line) in rdr.lines().enumerate() {
+        let line = line.unwrap();
+        for (x, c) in line.chars().enumerate() {
+            let (x, y) = (x as i32, y as i32);
+            starmap.insert((x, y), c.try_into().unwrap());
+        }
+    }
+    let max_los = *starmap
+        .iter()
+        .filter(|(_, &v)| v == Spot::Asteroid)
+        .max_by_key(|(&p, _)| get_los(p, &starmap))
+        .unwrap()
+        .0;
+
+    println!("Part 1: {:?}", get_los(max_los, &starmap));
+
+    let mut cnt = 0;
+    loop {
+        let hitlist = ordered_hitlist(max_los, &starmap);
+        for hit in hitlist {
+            starmap.insert(hit, Spot::Open);
+            cnt += 1;
+            if cnt == 200 {
+                println!("Part 2: {}", hit.0 * 100 + hit.1);
+                return;
+            }
+        }
+    }
+}
